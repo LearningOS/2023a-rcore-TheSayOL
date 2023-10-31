73711c07175f73eacb5b289d9fbf2640cadb2462
Your Name <you@example.com>
2023-10-31 11:50:16 +0800

    format trap/mod.rs

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -51,13 +51,11 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
                                // trace!("into {:?}", scause.cause());
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
-
             // new code: for TaskInfo
-            // 
+            //
             // set tcb.syscall_times[syscall_id] += 1;
             crate::task::set_current_syscall_times(cx.x[17]);
-            // new code end 
-
+            // new code end
 
             // jump to next instruction anyway
             cx.sepc += 4;
